6ea8e6588bf41ee29a7e6d2fde08d1b3f0c9895c
amaliedilling <apdilling@gmail.com>
2026-09-10 11:19:53 +0200

    test

---
+++ src/test/java/dk/itu/datasys/EngineTest.java
@@ -0,0 +1,11 @@
+package dk.itu.datasys;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class EngineTest {
+    @Test
+    void teamName() {
+        assertEquals("Team team2", new Engine().teamName());
+    }
+}
