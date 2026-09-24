6f1bc8a7f19cc566268d336bf2e826c0783ed871
Anders Juul Ingerslev <aing@itu.dk>
2026-09-24 13:44:19 +0200

    Added logging to parse()

--- src/main/java/dk/itu/datasys/SqlParser.java
+++ src/main/java/dk/itu/datasys/SqlParser.java
@@ -5,22 +5,48 @@ import java.util.List;
 import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import dk.itu.datasys.sql.SqlLexer;
 
 public final class SqlParser {
-    public List<Statement> parse(String sqlText) {
-        SqlLexer lexer = new SqlLexer(CharStreams.fromString(sqlText));
-        ThrowingErrorListener errorListener = new ThrowingErrorListener();
-        lexer.removeErrorListeners();
-        lexer.addErrorListener(errorListener);
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SqlParser.class);
 
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        dk.itu.datasys.sql.SqlParser generatedParser =
-            new dk.itu.datasys.sql.SqlParser(tokens);
-        generatedParser.removeErrorListeners();
-        generatedParser.addErrorListener(errorListener);
-        return new SqlAstBuilder().visitScript(generatedParser.script());
+    public List<Statement> parse(String sqlText) {
+        long start = System.nanoTime(); //Timer for logging
+
+        try {
+            SqlLexer lexer = new SqlLexer(CharStreams.fromString(sqlText));
+            ThrowingErrorListener errorListener = new ThrowingErrorListener();
+
+            lexer.removeErrorListeners();
+            lexer.addErrorListener(errorListener);
+
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            dk.itu.datasys.sql.SqlParser generatedParser =
+                    new dk.itu.datasys.sql.SqlParser(tokens);
+
+            generatedParser.removeErrorListeners();
+            generatedParser.addErrorListener(errorListener);
+
+            List<Statement> statements =
+                    new SqlAstBuilder().visitScript(generatedParser.script());
+
+            LOGGER.debug("statements={} durationMs={}",
+                    statements.size(), durationMs(start));
+
+            return statements;
+        } catch (SqlParseException exception) {
+            LOGGER.error("failed line={} col={} durationMs={}",
+                    exception.line(), exception.column(), durationMs(start));
+            throw exception;
+        }
+    }
+
+    private static long durationMs(long start) {
+        return (System.nanoTime() - start) / 1_000_000;
     }
 
     private static final class ThrowingErrorListener extends BaseErrorListener {
